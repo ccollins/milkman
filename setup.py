2b3e83dc81e8dd9065a5e04e9a3b1575ec107cde
Chuck Collins <chuck.collins@gmail.com>
2010-07-22 09:31:14 -0500

    removed django requirement in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 
 setup(name="milkman",
-    version="0.2.1",
+    version="0.2.2",
     packages = ["milkman"],
     author="Wilkes Joiner, Chuck Collins",
     author_email="chuck.collins@gmail.com",
-    install_requires=["docutils>=0.3","django"],
+    install_requires=["docutils>=0.3"],
     license = "MIT",
     keywords = "django testing mock stub",
     description = "Testing django without all the fixtures",
